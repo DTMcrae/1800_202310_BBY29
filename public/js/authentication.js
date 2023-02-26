dae520911dcf8fd6b90fbcccb5eed094eefaf10d
Leroy Lau <llau65@my.bcit.ca>
2023-02-25 19:39:09 -0800

    Relocated login.html and js files to appropriate locations, and fixed file paths

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -17,7 +17,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'main.html',
+    signInSuccessUrl: 'main',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
