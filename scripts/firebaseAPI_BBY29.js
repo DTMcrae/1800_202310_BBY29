785a4533e20ae37e211a4ee4786650d21643b704
Daniel McRae <dtmcrae0@gmail.com>
2023-02-25 18:35:34 -0800

    Centered the request cards on main.html

---
+++ scripts/firebaseAPI_BBY29.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBWoK61C_LwEt-1lgdqwNnzvFVUaWyX2dc",
+  authDomain: "request-f3da5.firebaseapp.com",
+  projectId: "request-f3da5",
+  storageBucket: "request-f3da5.appspot.com",
+  messagingSenderId: "356892671605",
+  appId: "1:356892671605:web:1dd92589a767c6981f823c"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
