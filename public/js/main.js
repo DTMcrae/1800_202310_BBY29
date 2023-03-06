58b673e9e3260d84c5a8e4c8103989bc11633ec3
Leroy Lau <llau65@my.bcit.ca>
2023-03-05 16:01:14 -0800

    Modified main.js to set span with id='username' to the user's name retrieved from Firestore

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,21 @@ function sayHello() {
     console.log("Hello there!")
 }
 sayHello();
+
+function insertNameFromFirestore() {
+    // to check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid); // let me to know who is the user that logged in to get the UID
+            currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+            currentUser.get().then(userDoc => {
+                //get the user name
+                var userName = userDoc.data().name;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); //jquery
+                document.getElementById("username").innerText = userName.split(' ')[0];;
+            })
+        }
+    })
+}
+insertNameFromFirestore()
