10440b8d553c1e679d38bc97b9dcd023a16461fe
Heesun Lee <hlee532@my.bcit.ca>
2023-03-09 09:34:53 -0800

    Update post.js (firebase env, but still in dev)

--- public/js/post.js
+++ public/js/post.js
@@ -1,4 +1,4 @@
-import { addOptions } from "./app/form.js";
+import { addOptions, isValidLength } from "./app/form.js";
 import { CATEGORY, URGENCY } from "./app/request.js";
 
 // initializer
@@ -38,8 +38,8 @@ const output = document.getElementById("upload-output");
 let imagesArray = [];
 
 const initAddPhoto = () => {
-  if(!input || !output) {
-    console.log(input, output)
+  if (!input || !output) {
+    console.log(input, output);
     return false;
   }
   const container = document.getElementsByClassName("add-photo");
@@ -47,8 +47,8 @@ const initAddPhoto = () => {
 
   input.addEventListener("change", () => {
     // Update imagesArray from selected images data
-    imagesArray = [...imagesArray,...input.files].slice(0,5);
-    console.log("imagesArray",imagesArray)
+    imagesArray = [...imagesArray, ...input.files].slice(0, 5);
+    console.log("imagesArray", imagesArray);
 
     // Show previews
     displayImages();
@@ -56,6 +56,9 @@ const initAddPhoto = () => {
     // Update added photos number
     document.getElementsByClassName("add-photo-number")[0].innerHTML =
       imagesArray.length;
+
+      console.log(imagesArray[0].name)
+      console.log(imagesArray[0].fullPath)
   });
 };
 
@@ -96,8 +99,16 @@ const checkValidation = ({
   detail = "",
   meetup = "",
 }) => {
-  let isValid = true;
+  let isValid = false;
+  if (!isValidLength(title,2,50)) {
+    return false;
+  }
+  if (!location(title,1,20)) {
+    return false;
+  }
 
+
+  isValid = true;
   return isValid;
 };
 
@@ -111,10 +122,42 @@ const submitPost = ({
   detail,
   meetup,
 }) => {
-  console.log(images,title,location)
+  console.log(
+    images,
+    title,
+    location,
+    urgency,
+    category,
+    detail,
+    meetup);
 
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      var currentUser = db.collection("users").doc(user.uid);
+      var userID = user.uid;
+      //get the document for current user.
+      currentUser.get().then((userDoc) => {
+        db.collection("requests")
+          .add({
+            images: images,
+            title: title,
+            location: location,
+            urgency: urgency,
+            category: category,
+            detail: detail,
+            meetup: meetup,
+          })
+          .then(() => {
+            alert("Submit, popup in dev");
+            // const popUp = new popUpClass();
+            // console.log("submit");
+            // window.location.href = "/request-detail.html"; 
+          });
+      });
+    } else {
+      console.log("No user is signed in");
+      window.location.href = "review.html";
+    }
+  });
 
-  // alert("Submit, popup in dev");
-  // const popUp = new popUpClass();
-  // console.log("submit");
 };
