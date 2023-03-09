bc0b18822a2dfb69d45f819623eb5b4ece390039
Daniel McRae <dtmcrae0@gmail.com>
2023-03-09 10:30:07 -0800

    Importable function to load the header/nav from templates

---
+++ public/js/standardized.js
@@ -0,0 +1,7 @@
+export function loadHeader(){
+    console.log($('.main-header').load('/html/templates/header.html'));
+}
+
+export function loadNav(){
+    console.log($('.main-nav').load('/html/templates/nav.html'));
+}
