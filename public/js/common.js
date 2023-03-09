edf0a2b8ff7a36c1a156f41bac84b0173ada7bdb
Daniel McRae <dtmcrae0@gmail.com>
2023-03-09 10:30:53 -0800

    Added import of standardized.js for ease of use

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,11 @@
-// import { PopUp } from "./app/popup.js";
-import { loadSkeleton } from "./requestSkeleton.js"
+import { loadSkeleton } from './requestSkeleton.js'
+import { loadNav } from "./standardized.js";
+import { loadHeader } from "./standardized.js";
 
 // initializer
 window.onload = function() {
   loadSkeleton();
+  loadNav();
+  loadHeader();
 };
 
