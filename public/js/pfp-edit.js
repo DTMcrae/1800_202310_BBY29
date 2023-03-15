6573a68fe867538903ecbfbc3b4f94f8568429ca
Leroy Lau <llau65@my.bcit.ca>
2023-03-15 16:29:14 -0700

    Added basic file validation to picture upload

--- public/js/pfp-edit.js
+++ public/js/pfp-edit.js
@@ -3,21 +3,26 @@ var storageRef = firebase.storage().ref();
 
 var loadFile = function (event) {
   var image = document.getElementById("profile-image");
-  image.src = URL.createObjectURL(event.target.files[0]);
   file = event.target.files[0];
-  let fileName = file.name;
+  
+  if (file !== undefined && file.type === ("image/png" || "image/jpeg" || "image/jpg")) {
 
-  firebase.auth().onAuthStateChanged(user => {
-    // Check if user is signed in:
-    if (user) {
-      let userID = user.uid;
-      storageRef.child(`${userID}/images/${fileName}`).put(file).then((snapshot) => {
-        console.log("Uploaded a file!");
-      });
-    } else {
-      // No user is signed in.
-      console.log("No user is signed in");
-    }
-  });
+    image.src = URL.createObjectURL(event.target.files[0]);
+    let fileName = file.name;
+    firebase.auth().onAuthStateChanged(user => {
+      // Check if user is signed in:
+      if (user) {
+        let userID = user.uid;
+        storageRef.child(`${userID}/images/${fileName}`).put(file).then((snapshot) => {
+          console.log("Uploaded a file!");
+        });
+      } else {
+        // No user is signed in.
+        console.log("No user is signed in");
+      }
+    });
+  } else {
+    alert("Not an image!");
+  }
 };
 
