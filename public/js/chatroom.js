add483eccc664bed18c123bac6eb52bc41498109
Daniel McRae <dtmcrae0@gmail.com>
2023-03-17 13:11:08 -0700

    Displays sent and received messages from the database asynchronously.

---
+++ public/js/chatroom.js
@@ -0,0 +1,114 @@
+firebase.auth().onAuthStateChanged((user) => {
+
+    if(!user) return;
+
+    let params = new URL( window.location.href ); //get URL of search bar
+    let ID = params.searchParams.get( "docID" ); //get value for key "id"
+    var userid = user.uid;
+
+    db.collection("chatrooms").doc(ID).get().then(doc => {
+        if(doc.data().userID.includes(userid))
+                {
+
+                $('#sentTemplate').load("/html/templates/sentTemplate.html");   //Load the template file
+                let sentTemplate = document.getElementById("sentTemplate"); //Load the request card template
+                
+                $('#receivedTemplate').load("/html/templates/receivedTemplate.html");   //Load the template file
+                let receivedTemplate = document.getElementById("receivedTemplate"); //Load the request card template
+
+                var recipientID = doc.data().userID[0];
+                if(recipientID == userid) recipientID = doc.data().userID[1];
+                var docID = doc.id;
+
+                UpdateHeader(doc, recipientID, userid);
+
+                db.collection("chatrooms").doc(ID).collection("messages").orderBy("time").onSnapshot(messages => {
+                    messages.forEach(message => {
+
+                        if(document.getElementById(message.id) != null) return;
+
+                        if(message.data().sender == recipientID)
+                        {
+                            let newcard = receivedTemplate.content.cloneNode(true);
+
+                            newcard.querySelector('.message-body').innerHTML = message.data().message;
+                            newcard.querySelector('.message-body').setAttribute("id",message.id);
+                            newcard.querySelector(".time").innerHTML = message.data().time;
+
+                            document.getElementById("messages-go-here").appendChild(newcard)
+                        }
+
+                        else
+                        {
+                            let newcard = sentTemplate.content.cloneNode(true);
+
+                            newcard.querySelector(".message-body").innerHTML = message.data().message;
+                            newcard.querySelector('.message-body').setAttribute("id",message.id);
+                            newcard.querySelector(".time").innerHTML = message.data().time;
+
+                            document.getElementById("messages-go-here").appendChild(newcard)
+                        }
+                    })
+                })
+            }
+    });
+
+    document.body.style.paddingBottom = '40px';
+
+});
+
+async function SetLocalData(doc, recipientID)
+{
+    var docID = doc.id;
+
+    await db.collection("requests").doc(doc.data().requestID).get().then(requestDoc => {
+        sessionStorage.setItem("requestName",requestDoc.data().title);
+        sessionStorage.setItem("requestDetails",requestDoc.data().details);
+    });
+
+    //Get the required user information from the dataBase;
+    await db.collection("users").doc(recipientID).get().then(userDoc => {
+        sessionStorage.setItem("recipientName", userDoc.data().name);
+        console.log("Recipient: " + sessionStorage.getItem("recipientName"));
+    });
+}
+
+async function UpdateHeader(doc, recipientid, userid)
+{
+    await SetLocalData(doc,recipientid);
+
+    var docID = doc.id;
+    let newcard = document.getElementById("message-details");
+
+    //update title and text and image
+    newcard.querySelector('.recipient').innerHTML = sessionStorage.getItem("recipientName");
+    newcard.querySelector('.request-name').innerHTML = "Request: " + sessionStorage.getItem("requestName");
+    newcard.querySelector('.request-details').innerHTML = sessionStorage.getItem("requestDetails");
+    newcard.querySelector('button').href = "/html/request-details.html?docID="+doc.data().requestID;
+
+    document.querySelector(".send-message").setAttribute("onclick","SubmitMessage(\"" + userid + "\")");
+}
+
+function SubmitMessage(userID) {
+
+    var messageToSend = document.getElementById("inputMessage").value;
+    let params = new URL( window.location.href ); //get URL of search bar
+    let ID = params.searchParams.get( "docID" ); //get value for key "id"
+
+    if(messageToSend == null || ID == null) return;
+
+
+    db.collection("chatrooms").doc(ID).collection("messages").add({         //write to firestore. We are using the UID for the ID in users collection
+        message: messageToSend,
+        sender: userID,              
+        time: new Date().toLocaleString()    
+      }).then(newDoc => {
+        console.log("Created new message with id: " + newDoc.id);
+        document.getElementById("inputMessage").value = "";
+        db.collection("chatrooms").doc(ID).update({
+            latestMessageID: newDoc.id
+        })
+      }).catch(function (error) {
+        console.log("Error sending message " + error);
+      });
+    }
