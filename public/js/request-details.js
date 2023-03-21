a15f137a0fa46480dc0b1122a13a95dcf3ab894b
Daniel McRae <dtmcrae0@gmail.com>
2023-03-21 15:54:43 -0700

    Requests can now be accepted. The delete request button that appears in requests you have created is currently non-functional.

--- public/js/request-details.js
+++ public/js/request-details.js
@@ -14,11 +14,11 @@ function displayRequestInfo() {
             requestCategory = thisRequest.category;
             requestLocation = thisRequest.location;
             requestUrgency = thisRequest.urgency;
-            requestDetails = thisRequest.details;
+            requestDetails = thisRequest.detail;
 
-            db.collection( "users" ).doc ( thisRequest.creator ).get().then(userDoc => {
+            db.collection( "users" ).doc ( thisRequest.user.uid ).get().then(userDoc => {
 
-                this.requesteeName = userDoc.data().name;
+                requesteeName = userDoc.data().name;
 
                 document.getElementById( "requestee-name" ).innerHTML = requesteeName;
             });
@@ -32,3 +32,126 @@ function displayRequestInfo() {
         } );
 }
 displayRequestInfo();
+
+firebase.auth().onAuthStateChanged((user) => {
+
+    if(!user) return;
+    
+    let params = new URL( window.location.href ); //get URL of search bar
+    let ID = params.searchParams.get( "docID" ); //get value for key "id"
+
+    db.collection("requests").doc(ID).get().then(doc => {
+
+        let acceptButton = document.querySelector(".requestButton");
+        var creatorID = doc.data().user.uid;
+
+        if(creatorID == user.id && creatorID != null)
+        {
+            acceptButton.innerHTML = "Delete Request";
+            return;
+        }
+
+        try
+        {
+            var acceptedUsers = doc.data().usersAccepted;
+
+            if(acceptedUsers.includes(user.uid))
+            {
+                console.log("User is present in acceptedUsers");
+                acceptButton.innerHTML = "Open Chat";
+
+                try
+                {
+                db.collection("chatrooms").get().then(chatrooms => {
+                    chatrooms.forEach(chatroom => {
+                        console.log(user.uid);
+                        console.log(chatroom.data().userID);
+
+                        if(chatroom.data().userID.includes(user.uid))
+                        {
+                            acceptButton.setAttribute("href","/html/chatroom.html?docID=" + chatroom.id);
+                            return;
+                        }
+                    });
+                });
+                }
+                catch
+                {
+                    //chatroom collection does not exist
+                    console.log("Chatroom collection does not exist")
+                }
+            }
+
+            else
+            {
+                console.log("Assigning data");
+                acceptButton.setAttribute("onclick","AcceptRequest(\"" + user.uid + "\",\"" + ID + "\")");
+            }
+        }
+        catch
+        {
+            //acceptedUsers field does not exist.
+            console.log("Request's acceptedUsers field does not exist")
+            console.log("Assigning data");
+            acceptButton.setAttribute("onclick","AcceptRequest(\"" + user.uid + "\",\"" + ID + "\")");
+        }
+    })
+
+});
+
+async function AcceptRequest(userid,requestid)
+{
+    //Save accepted request to the user donezo
+    //Add userid to the request's acceptedUsers donezo
+    //Create a chatroom for the user and the recipient
+    //Assign the chatroom's ID, userId and creatorID to the chatrooms collection in the request
+
+    await db.collection("requests").doc(requestid).get().then(doc => {
+
+        try
+        {
+            if(doc.data().acceptedUsers.includes(userid))
+            {
+                sessionStorage.setItem("accepted",true);
+            }
+            else
+            {
+                sessionStorage.setItem("accepted",false);
+            }
+        }
+        catch
+        {
+            sessionStorage.setItem("accepted",false);
+        }
+    });
+
+    if(! sessionStorage.getItem("accepted")) return;
+
+    db.collection("users").doc(userid).update(
+    {
+        requestsAccepted: firebase.firestore.FieldValue.arrayUnion(requestid)
+    });
+
+    db.collection("requests").doc(requestid).update(
+    {
+        usersAccepted: firebase.firestore.FieldValue.arrayUnion(userid)
+    });
+
+db.collection("requests").doc(requestid).get().then(doc =>{
+    var creatorID = doc.data().user.uid;
+
+    db.collection("chatrooms").add({
+        latestMessageID: "",
+        requestID: requestid,
+        userID: [creatorID, userid]
+    }).then(chatdoc => {
+        chatdoc.collection("messages").add({
+            message:"System Message [DO NOT REMOVE]",
+            sender: "",
+            time: new Date().toLocaleString()
+        })
+        window.location.assign("/html/chatroom.html?docID=" + chatdoc.id);
+    })
+});
+    
+}
