ec6bdf64a050226e8b3c36f8a5bdc2789c542e5d
Heesun Lee <hlee532@my.bcit.ca>
2023-03-27 20:11:56 -0700

    Update notification.js to prevent error

--- public/js/notification.js
+++ public/js/notification.js
@@ -9,23 +9,29 @@ UpdateNotification(user);
 
 async function UpdateNotification(user)
 {
-    console.log("Function called");
-    await db.collection("chatrooms").where("userID","array-contains",user.uid).get().then(chatrooms => {
-        chatrooms.forEach(doc => {
+    try {
+        console.log("Function called");
+        await db.collection("chatrooms").where("userID","array-contains",user.uid).get().then(chatrooms => {
+            chatrooms.forEach(doc => {
+    
+                var lastRead = doc.data().lastRead[user.uid];
+                var latestMessageID = doc.data().latestMessageID;
 
-            var lastRead = doc.data().lastRead[user.uid];
-            var latestMessageID = doc.data().latestMessageID;
-        
-            db.collection("chatrooms").doc(doc.id).collection("messages").doc(latestMessageID).get().then(message => {
-        
-                if((message.data().time > lastRead && message.data().sender != user.uid) || lastRead == undefined && message.data().sender != user.uid)
-                {
-                    if(document.querySelector(".chat-notification") != null) return;
-                    let notification = document.createElement("span");
-                    notification.setAttribute("class","chat-notification");
-                    document.querySelector(".navitem").appendChild(notification)
-                }
-            })
-        });
-    })
+                if (!latestMessageID) return;
+                
+                db.collection("chatrooms").doc(doc.id).collection("messages").doc(latestMessageID).get().then(message => {
+            
+                    if((message.data().time > lastRead && message.data().sender != user.uid) || lastRead == undefined && message.data().sender != user.uid)
+                    {
+                        if(document.querySelector(".chat-notification") != null) return;
+                        let notification = document.createElement("span");
+                        notification.setAttribute("class","chat-notification");
+                        document.querySelector(".navitem").appendChild(notification)
+                    }
+                })
+            });
+        })
+    } catch (e) {
+        console.error(e);
+    }
 }
