950239f0a60cc033bad1efa054d13d45428e072d
Leroy Lau <llau65@my.bcit.ca>
2023-04-04 14:59:11 -0700

    Decreased the delay for autocomplete

--- public/js/address-autocomplete.js
+++ public/js/address-autocomplete.js
@@ -1,6 +1,6 @@
 function addressAutocomplete(containerElement, callback, options) {
   const MIN_ADDRESS_LENGTH = 6;
-  const DEBOUNCE_DELAY = 500;
+  const DEBOUNCE_DELAY = 300;
 
   // create container for input element
   const inputContainerElement = document.createElement("div");
